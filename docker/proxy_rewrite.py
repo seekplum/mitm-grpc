e5ddf75cfa0be0582aa52e8160282b9ce2e0049e
huangjiandong <huangjiandong@meideng.net>
2021-06-08 20:42:39 +0800

    WIP

--- docker/proxy_rewrite.py
+++ docker/proxy_rewrite.py
@@ -1,11 +1,17 @@
+# import collections
+# import random
+# from enum import Enum
+
 from mitmproxy import ctx, http, tcp
 from mitmproxy.connections import ClientConnection, ServerConnection
+# from mitmproxy.exceptions import TlsProtocolException
 from mitmproxy.net.http import Request
-from mitmproxy.utils import strutils
+# from mitmproxy.proxy.protocol import RawTCPLayer, TlsLayer
+# from mitmproxy.utils import strutils
 
 
 def tcp_message(flow: tcp.TCPFlow):
-    message: tcp.TCPMessage = flow.messages[-1]
+    # message: tcp.TCPMessage = flow.messages[-1]
     server_conn: ServerConnection = flow.server_conn
     client_conn: ClientConnection = flow.client_conn
     flag = "*" * 10
@@ -14,12 +20,20 @@ def tcp_message(flow: tcp.TCPFlow):
         "client_conn": client_conn,
         "server_conn.address": server_conn.address,
         "server_conn": server_conn,
+        "server_conn.sni": server_conn.sni,
     }
-    if server_conn.address in [("mitmdump", 8080), ("127.0.0.1", "8080")]:
+    ctx.log.info(f"{flag} tcp_message extra_data: {extra_data} {flag}")
+    if (
+        server_conn.address
+        in [
+            ("127.0.0.1", 80),
+            ("grpc.seekplum.top", 80),
+        ]
+        or server_conn.sni == "grpc.seekplum.top"
+    ):
         server_conn.address = ("127.0.0.1", 80)
-        content = strutils.bytes_to_escaped_str(message.content)
-        extra_data.update(content=content)
-        ctx.log.info(f"{flag} tcp_message: {extra_data} {flag}")
+        # content = strutils.bytes_to_escaped_str(message.content)
+        # ctx.log.info(f"{flag} tcp_message content: {content} {flag}")
 
 
 def http_connect(flow: http.HTTPFlow) -> None:
@@ -31,14 +45,19 @@ def http_connect(flow: http.HTTPFlow) -> None:
     is_grpc_request: bool = len(agent_info) == 2 and agent_info[0].startswith("grpc-")
     extra_data: dict = {
         "user_agent": user_agent,
-        "first_line_format": r.first_line_format,
+        # "first_line_format": r.first_line_format,
         "method": r.method,
         "scheme": r.scheme,
-        "authority": r.authority,
-        "data": r.data,
+        # "authority": r.authority,
+        # "data": r.data,
     }
     if is_grpc_request:
-        r.host = "grpc.seekplum.top"
+        # r.host, r.port = "grpc.seekplum.top", 80
+        # original_scheme = r.scheme
+        # r.scheme = "http"
+        r.host, r.port = "127.0.0.1", 80
+        # r.headers["Host"] = "grpc.seekplum.top"
+        # r.headers["X-Forwarded-Proto"] = original_scheme
         ctx.log.info(f"{flag} http_connect: {extra_data} {flag}")
 
 
--- mygrpc/keys/genkey.sh
+++ mygrpc/keys/genkey.sh
@@ -1,16 +1,26 @@
 #!/bin/bash
 
-set -e
+set -xe
 
 ROOT_DIR="$( cd "$( dirname "$BASH_SOURCE[0]" )" && pwd )"
 cd "${ROOT_DIR}"
 echo "Compiling protos..."
 
-# 生成 RSA 私钥
-openssl genrsa -out key.pem 2048
+# # 生成 RSA 私钥
+# openssl genrsa -out key.pem 2048
 
-# 生成 RSA 公钥, 除 Common Name 处必须填入 grpc.seekplum.top (和 grpc.ssl_target_name_override 变量一致) 外，其它直接回车即可
-openssl req -new -x509 -key key.pem -out cert.pem -subj "/CN=grpc.seekplum.top"
-# openssl req -new -x509 -sha256 -key key.pem -out cert.pem -days 3650 -subj "/CN=grpc.seekplum.top"
+# # 生成 RSA 公钥, 除 Common Name 处必须填入 grpc.seekplum.top (和 grpc.ssl_target_name_override 变量一致) 外，其它直接回车即可
+# openssl req -new -x509 -key key.pem -out cert.pem -subj "/CN=grpc.seekplum.top"
+# # openssl req -new -x509 -sha256 -key key.pem -out cert.pem -days 3650 -subj "/CN=grpc.seekplum.top"
 
-cat key.pem cert.pem > mitm.pem
+# cat key.pem cert.pem > mitm.pem
+
+
+openssl genrsa -out rootCA.key 4096 -subj "/CN=grpc.seekplum.top"
+openssl req -x509 -new -nodes -key rootCA.key -sha256 -days 1024 -out rootCA.crt
+openssl genrsa -out localhost.key 2048
+openssl req -new -key localhost.key -out localhost.csr -subj "/CN=grpc.seekplum.top"
+openssl req -in localhost.csr -noout -text
+openssl x509 -req -in localhost.csr -CA rootCA.crt -CAkey rootCA.key -CAcreateserial -out localhost.crt -days 500 -sha256
+openssl x509 -in localhost.crt -text -noout
+cat localhost.crt localhost.key > localhost.pem
